a850852b8a3f20f31e5bc606423818d9f6146997
isaacp84 <isaacpolly84@gmail.com>
2025-08-28 12:13:35 -0500

    graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/isaacp84/go_server_1/internal/game"
@@ -16,100 +17,46 @@ import (
 // var udp_running atomic.Bool
 
 func run(ctx context.Context, w io.Writer, args []string) error {
-	sigChan, _ := signal.NotifyContext(ctx, os.Interrupt)
+	ctx, cancel := context.WithCancel(ctx)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 
 	fmt.Println("Hello world")
 
-	// http.Handle("/", routers.Root{})
-	// log.Fatal(http.ListenAndServe(":8080", nil))
-
 	var my_game game.Game
-	tickDuration := time.Second / 10
-	ticker := time.NewTicker(tickDuration)
-	defer ticker.Stop()
 
 	my_game.Players()
 
+	// Channel to send received UDP packets
+	packetChan := make(chan []byte)
 	conn, _ := startUDPConn()
 	// Close the connection when we're done
 	defer conn.Close()
 
-	// Channel to send received UDP packets
-	packetChan := make(chan []byte)
+	my_game.LinkUDP(conn)
+
 	// Channel to signal shutdown
 	// doneChan := make(chan struct{})
 
 	// Accept incoming connections and handle them
 	// Goroutine to read UDP packets
-	go func() {
-
-		buffer := make([]byte, 512)
-		for {
-			select {
-			case <-ctx.Done():
-				fmt.Println("Server context cancelled, stopping UDP listener.")
-				return
-			// case <-doneChan:
-			// 	fmt.Println("UDP reader goroutine exiting.")
-			// 	conn.Close()
-			// 	return
-			default:
-				// Read incoming data
-				n, addr, err := conn.ReadFromUDP(buffer)
-				if err != nil {
-					// Handle temporary errors or log persistent ones
-					if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-						continue // It's a timeout, try again
-					}
-					log.Printf("Error reading from UDP: %v", err)
-					continue
-				}
-
-				// handleConnection(conn, addr, buffer)
-
-				// Send a copy of the received data to avoid data races
-				data := make([]byte, n)
-				copy(data, buffer[:n])
-				packetChan <- data
-				fmt.Printf("Received %d bytes from %s: %s\n", n, addr.String(), string(data))
-			}
-		}
-	}()
+	go ReadUDPPackets(ctx, packetChan, conn)
 
 	// Game loop
-	// go func() error {
-	// 	for {
-	// 		select {
-	// 		case <-ctx.Done():
-	// 			fmt.Println("Server context cancelled, stopping game routine.")
-	// 			return nil
-	// 		case packet := <-packetChan:
-	// 			fmt.Printf("Processing packet: %s\n", string(packet))
-	// 			switch string(packet[:4]) {
-	// 			case "JN  ":
-	// 				name := string(packet[4:19])
-	// 				p := game.Player{Name: name}
-	// 				fmt.Println(p)
-	// 				my_game.AddPlayer(p)
-	// 				log.Printf("Player joined: %s", name)
+	go my_game.Run(ctx, packetChan)
 
-	// 			case "LV  ":
-	// 			default:
-	// 			}
-
-	// 		case <-ticker.C:
-	// 			// Game code
-	// 			my_game.Update(conn)
-
-	// 			// fmt.Println("Game tick.")
-	// 		}
-	// 	}
-	// }()
-
-	<-sigChan.Done()
+	<-sigChan
 	fmt.Println("Received termination signal, shutting down...")
 	// close(doneChan)
-	// cancel()
+	cancel()
+
+	select {
+	case <-time.After(5 * time.Second):
+		fmt.Println("Graceful shutdown timeout exceeded. Exiting forcefully.")
+	case <-ctx.Done():
+		fmt.Println("Server gracefully shutdown.")
+	}
 
 	return nil
 }
@@ -123,18 +70,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	select {
-	case <-time.After(5 * time.Second):
-		fmt.Println("Graceful shutdown timeout exceeded. Exiting forcefully.")
-	case <-ctx.Done():
-		fmt.Println("Server gracefully shutdown.")
-	}
 }
 
 func startUDPConn() (*net.UDPConn, *net.UDPAddr) {
 	// Resolve the string address to a UDP address
 	addr, err := net.ResolveUDPAddr("udp", ":27015")
-
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -147,11 +87,37 @@ func startUDPConn() (*net.UDPConn, *net.UDPAddr) {
 		os.Exit(1)
 		return nil, nil
 	}
-
-	// udp_running.Store(true)
 	fmt.Println("started connection")
 	return conn, addr
+}
 
+func ReadUDPPackets(ctx context.Context, packetChan chan []byte, conn *net.UDPConn) {
+	buffer := make([]byte, 512)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Server context cancelled, stopping UDP listener.")
+			conn.Close()
+			return
+		default:
+			// Read incoming data
+			n, addr, err := conn.ReadFromUDP(buffer)
+			if err != nil {
+				// Handle temporary errors or log persistent ones
+				if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+					continue // It's a timeout, try again
+				}
+				log.Printf("Error reading from UDP: %v", err)
+				continue
+			}
+
+			// Send a copy of the received data to avoid data races
+			data := make([]byte, n)
+			copy(data, buffer[:n])
+			packetChan <- data
+			fmt.Printf("Received %d bytes from %s: %s\n", n, addr.String(), string(data))
+		}
+	}
 }
 
 // func handleConnection(conn *net.UDPConn, addr *net.UDPAddr, buf []byte) {
--- internal/game/game.go
+++ internal/game/game.go
@@ -1,14 +1,63 @@
 package game
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net"
+	"time"
 )
 
 type Game struct {
 	players []Player
 	Ball    Ball
 	Paddles []Paddle
+	conn    *net.UDPConn
+}
+
+func (g *Game) LinkUDP(c *net.UDPConn) {
+	g.conn = c
+}
+
+func (g *Game) Run(ctx context.Context, packetChan chan []byte) error {
+	if g.conn == nil {
+		return nil
+	}
+
+	tickDuration := time.Second / 10
+	ticker := time.NewTicker(tickDuration)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Server context cancelled, stopping game routine.")
+			return nil
+		case packet := <-packetChan:
+			g.handleUDPPacket(packet)
+
+		case <-ticker.C:
+			// Game code
+			g.Update(g.conn)
+
+			// fmt.Println("Game tick.")
+		}
+	}
+}
+
+func (g *Game) handleUDPPacket(packet []byte) {
+	fmt.Printf("Processing packet: %s\n", string(packet))
+	switch string(packet[:4]) {
+	case "JN  ":
+		name := string(packet[4:19])
+		p := Player{Name: name}
+		fmt.Println(p)
+		g.AddPlayer(p)
+		log.Printf("Player joined: %s", name)
+
+	case "LV  ":
+	default:
+	}
 }
 
 func (g *Game) Players() []Player {
